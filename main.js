9133e09161023a9962638baee1824f7106dc5b6b
Hulikner <hulikner@gmail.com>
2022-02-17 11:31:46 -0600

    Build a Calculator

---
+++ main.js
@@ -0,0 +1,48 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+const subtract = (firstNumber, secondNumber) => {
+    const sum = firstNumber - secondNumber
+    return sum
+}
+
+const divide = (firstNumber, secondNumber) => {
+    const sum = firstNumber / secondNumber
+    return sum
+}
+
+const multiply = (firstNumber, secondNumber) => {
+    const sum = firstNumber * secondNumber
+    return sum
+}
+
+const square = (firstNumber) => {
+    const sum = firstNumber * firstNumber
+    return sum
+}
+
+
+
+
+
+let result = add(17, 9)
+console.log(result)
+
+result = subtract(-13, -7)
+console.log(result)
+
+result = divide(4, 58)
+console.log(result)
+
+result = multiply(4, 58)
+console.log(result)
+
+result = square(4)
+console.log(result)
+
+// 60 = 2(.75x) + 4(.25x)
+// 60 = 1.55x + x
+// 60 = 2.5x
+// 24
